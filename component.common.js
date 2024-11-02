25fc17534856468a3314edeeb76fd4804eef71f7
pauldar24 <pauldavid.anampa@gmail.com>
2024-11-02 04:37:21 -0500

    pc4

--- component.common.js
+++ component.common.js
@@ -18,30 +18,30 @@ const Welcome = () => {
 const Navigation = () => {
   return (
       <nav style={{ backgrounColor: "#F0B941" }} className="p-4">
-        <img src="../IMAGENES/PÁGINA/logo.jpeg"/>
+        <img src="IMAGENES/PÁGINA/logo.jpeg"/>
         <ul className="flex space-x-4 justify-center">
           <li>
-            <a href="../index.html" className="text-white hover:text-blue-400">
+            <a href="index.html" className="text-white hover:text-blue-400">
               Inicio
             </a>
           </li>
           <li>
-            <a href="../products.html" className="text-white hover:text-blue-400">
+            <a href="products.html" className="text-white hover:text-blue-400">
               Productos
             </a>
           </li>
           <li>
-            <a href="../contacto.html" className="text-white hover:text-blue-400">
+            <a href="contacto.html" className="text-white hover:text-blue-400">
               Contacto
             </a>
           </li>
           <li>
-            <a href="../about.html" className="text-white hover:text-blue-400">
+            <a href="about.html" className="text-white hover:text-blue-400">
               Nosotros
             </a>
           </li>
           <li>
-            <a href="../novedades.html" className="text-white hover:text-blue-400">
+            <a href="novedades.html" className="text-white hover:text-blue-400">
               Novedades
             </a>
           </li>
@@ -59,10 +59,11 @@ const Footer = () => {
       <p className="text-sm">
       <h4>NAVEGACIÓN:</h4>
       <ul>
-        <li><a href="../HTML/index.html">Inicio</a></li>
-        <li><a href="../HTML/about.html">Nosotros</a></li>
-        <li><a href="../HTML/contacto.html">Contacto</a></li>
-        <li><a href="../HTML/alltshirts.html">Productos</a></li>
+        <li><a href="index.html">Inicio</a></li>
+        <li><a href="products.html">Productos</a></li>
+        <li><a href="contacto.html">Contacto</a></li>
+        <li><a href="about.html">Nosotros</a></li>
+        <li><a href="novedades.html">Novedades</a></li>
       </ul>
         <p><span>UBÍCANOS EN:</span> Av. La Molina</p>
       </p>
@@ -88,7 +89,7 @@ const About = () => {
         </p>
         </div>
         <h1 className="text-blue-900 text-3xl font-bold m-5">UBÍCANOS EN</h1>
-        <img src="../IMAGENES/PÁGINA/ubicacion.jpeg" className="mx-auto mb-10 rounded-lg shadow-md w-1/2"/>
+        <img src="IMAGENES/PÁGINA/ubicacion.jpeg" className="mx-auto mb-10 rounded-lg shadow-md w-1/2"/>
     </body>
   );
 }
@@ -100,19 +101,19 @@ const Novedades = () => {
   <ul className="list-disc pl-6 list-none text-center">
       <li className="text-lg font-bold py-3">FC Barcelona Tercer equipación 2024 - S/250
         <img 
-        src="../IMAGENES/NOVEDADES/barcelona-verde.jpeg"
+        src="IMAGENES/NOVEDADES/barcelona-verde.jpeg"
         className="mx-auto mb-4 rounded-lg shadow-md w-1/2 m-7"
         />
       </li>
       <li className="py-8 text-lg font-bold">Alianza Lima Mes morado 2024 - S/220
         <img
-        src="../IMAGENES/NOVEDADES/alianzalima.jpg"
+        src="IMAGENES/NOVEDADES/alianzalima.jpg"
         className="mx-auto mb-4 rounded-lg shadow-md w-1/2 m-7 py-0"
         />
       </li>
       <li className="text-lg font-bold">Bayern Munich Primera Equipación 2024 - S/250
         <img
-        src="../IMAGENES/NOVEDADES/bayern.jpg"
+        src="IMAGENES/NOVEDADES/bayern.jpg"
         className="mx-auto mb-4 rounded-lg shadow-md w-1/2 m-7"
         />
       </li>
